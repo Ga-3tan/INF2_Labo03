a57b3e317fc23ea7688bb1ebe104f15df3c4c45f
miguel <miguel.dovalelopes@heig-vd.ch>
2020-03-06 16:06:03 +0100

    Version 1, de la classe cartes -surcharge operateur de flux(<<) et de comparaison(==)

--- Carte.cpp
+++ Carte.cpp
@@ -1,5 +1,23 @@
-//
-// Created by gaeta on 06.03.2020.
-//
+/*
+ -----------------------------------------------------------------------------------
+ Laboratoire : 03
+ Fichier     : Carte.cpp
+ Auteur(s)   : Miguel Do Vale Lopes
+ Date        : 06.03.2020
 
+ But         : <à compléter>
+
+ Remarque(s) : <à compléter>
+
+ Compilateur : g++ 7.4.0
+ -----------------------------------------------------------------------------------
+*/
 #include "Carte.h"
+
+bool Carte::operator==(const Carte& carte) const {
+    return this->famille == carte.famille;
+}
+
+std::ostream &operator<<(std::ostream& lhs, const Carte& rhs) {
+    return lhs << rhs.famille << rhs.membre;
+}
--- Carte.h
+++ Carte.h
@@ -1,13 +1,47 @@
-//
-// Created by gaeta on 06.03.2020.
-//
+/*
+ -----------------------------------------------------------------------------------
+ Laboratoire : 03
+ Fichier     : Carte.h
+ Auteur(s)   : Miguel Do Vale Lopes
+ Date        : 06.03.2020
+
+ But         : <à compléter>
+
+ Remarque(s) : <à compléter>
+
+ Compilateur : g++ 7.4.0
+ -----------------------------------------------------------------------------------
+*/
 
 #ifndef INF2_LABO03_CARTE_H
 #define INF2_LABO03_CARTE_H
 
+#include <iostream>
 
 class Carte {
 
+    /**
+     * @brief Surcharge d'operateur de flux (<<), permet l'affichage d'une carte.
+     * @param lhs le flux ostream
+     * @param rhs la carte à afficher
+     * @return le nouveau flux ostream
+     * @details Format: (unsigned short)famille (char)membre,
+     *          Exemple: famille 2, membre C -> "2C"
+     */
+    friend std::ostream& operator<< (std::ostream& lhs, const Carte& rhs);
+
+public:
+
+    /**
+     * @brief Compare la famille des deux carte
+     * @param carte la carte à comparer de type Carte
+     * @return un booléen, true si les familles sont les mêmes
+     */
+    bool operator== (const Carte& carte) const;
+
+private:
+    unsigned short famille;
+    char membre;
 };
 
 
