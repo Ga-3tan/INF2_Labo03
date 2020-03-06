0279514ea36a9a6372c9fe5befdfc374fc582779
AkiakiFromMotherland <rebecca.tevaearai@heig-vd.ch>
2020-03-06 16:25:02 +0100

    Début de la classe Joueur

--- Joueur.cpp
+++ Joueur.cpp
@@ -1,5 +1,37 @@
-//
-// Created by gaeta on 06.03.2020.
-//
+/* ---------------------------
+Laboratoire : 03
+Fichier :
+Auteur(s) : Rébecca Tevaearai, Gaeta, Miguela
+Date : 06.03.2020
+
+But :
+
+Remarque(s) :
+
+Compilateur : gcc
+
+--------------------------- */
 
 #include "Joueur.h"
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void Joueur::demanderCarte() {
+
+   //cherche la famille la plus remplie possédée
+   //donner un numero de carte non-possédée
+
+}
+void Joueur::detecterFamille() {
+
+    //verfifier vide
+}
+Joueur::Joueur(string nom) : nom(nom) {}
+
+bool Joueur::mainVide() {
+    if(cartesEnMain.empty())
+        return true;
+}
