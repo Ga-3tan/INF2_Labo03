fe07b7a9d7ec32b5be1771b56662c7d8a7497431
miguel <miguel.dovalelopes@heig-vd.ch>
2020-03-10 11:39:03 +0100

    Genere le jeu de cartes en debut de partie.

--- Partie.cpp
+++ Partie.cpp
@@ -2,8 +2,9 @@
 // Created by gaeta on 06.03.2020.
 //
 
-#include <c++/4.8.3/cstdlib>
+#include <cstdlib>
 #include "Partie.h"
+#include "ParametresJeu.h"
 
 Joueur Partie::getRandomPlayer() {
     return joueurs.at(rand() % joueurs.size());
@@ -35,6 +36,13 @@ bool Partie::gameLoop() {
 }
 
 void Partie::startGame() {
+    // Creer le jeu de cartes
+    for (unsigned short i = 1; i <= NOMBRE_FAMILLES; ++i) {
+        for (char c = 'A'; c < 'A' + CARTES_PAR_FAMILLE; ++c) {
+            cartesEnJeu.emplace_back(i, c);
+        }
+    }
+
     bool game;
     do {
         game = gameLoop();
