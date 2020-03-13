1e4c8ce7eb0b591665e2e5cb83c62c6fc5a9849a
miguel <miguel.dovalelopes@heig-vd.ch>
2020-03-13 11:09:36 +0100

    petite modifs sur partie.h et .cpp

--- Partie.cpp
+++ Partie.cpp
@@ -2,16 +2,18 @@
 // Created by gaeta on 06.03.2020.
 //
 
-#include <cstdlib>
 #include "Partie.h"
 #include "ParametresJeu.h"
+#include <random>
+#include <vector>
+#include <algorithm>
 
-Joueur& Partie::getRandomPlayer() {
-    return joueurs.at(rand() % joueurs.size());
-}
+using namespace std;
 
-bool Partie::gameFinished() {
-    for (Joueur joueur : joueurs) {
+Joueur& Partie::getRandomPlayer() { return joueurs.at(rand() % joueurs.size()); }
+
+bool Partie::gameFinished() const {
+    for (const Joueur& joueur : joueurs) {
         if (!joueur.mainVide()) {
             return false;
         }
@@ -19,9 +21,7 @@ bool Partie::gameFinished() {
     return pile.empty();
 }
 
-Carte Partie::pickCardFromPile() {
-    return *pile.end();
-}
+Carte Partie::piocherCarte() { return *pile.end(); }
 
 bool Partie::gameLoop() {
     for (Joueur& joueur : joueurs) {
@@ -30,7 +30,7 @@ bool Partie::gameLoop() {
                 joueur.detecterFamille();
             } while (joueur.demanderCarte(getRandomPlayer()));
             if (!pile.empty()) {
-                joueur.ajouterCarte(pickCardFromPile());
+                joueur.ajouterCarte(piocherCarte());
             }
             joueur.detecterFamille();
         } else {
@@ -40,20 +40,41 @@ bool Partie::gameLoop() {
     return !gameFinished();
 }
 
+
 void Partie::startGame() {
-    // Creer le jeu de cartes
+
+    // Cree le jeu de cartes
+    vector<Carte> jeuCartes;
     for (unsigned short i = 1; i <= NOMBRE_FAMILLES; ++i) {
         for (char c = 'A'; c < 'A' + CARTES_PAR_FAMILLE; ++c) {
-            cartesEnJeu.emplace_back(i, c);
+            jeuCartes.emplace_back(i, c);
         }
     }
 
-    bool game;
-    do {
-        game = gameLoop();
-    } while (game);
+    // Melange les cartes
+    shuffle(jeuCartes.begin(), jeuCartes.end(), mt19937(random_device()()));
+
+    // Distribution des cartes
+    for (Joueur& joueur : joueurs) {
+        for (unsigned i = 0; i < CARTES_PAR_JOUEUR; i++) {
+            joueur.ajouterCarte(jeuCartes.at(i));
+            jeuCartes.erase(jeuCartes.begin() + i);
+        }
+    }
+    pile = jeuCartes;
+
+    // Boucle de jeu
+    while (gameLoop());
 }
 
-void Partie::endGame() {
 
+void Partie::endGame() {/*
+    Joueur& gagnant = joueurs.at(0);
+    for (Joueur joueur : joueurs) {
+        // TODO COMPTER LES NOMBRES DE FAMILLES AVEC LA VARIABLE FAMILLE_POSEE ET LES RAJOUTER AU SCORE
+        if ( NOMBRE_DE_FAMILLE_DU_JOUEUR > gagnant.NOMBRE_DE_FAMILLE) {
+            gagnant = joueur;
+        }
+    }
+    std::cout << "le gagnant est : " << gagnant.getNom() << endl;*/
 }
